8a9d6675d97fbf76c391e6a7cf6ad9da1050f7a2
Rafa548 <rafael.vilaca@ua.pt>
2023-11-11 18:47:28 +0000

    code cleanup

--- newsapp/chat/consumers.py
+++ newsapp/chat/consumers.py
@@ -12,7 +12,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 	async def disconnect(self , close_code):
 		await self.channel_layer.group_discard(
 			self.roomGroupName , 
-			self.channel_layer 
+			self.channel_name 
 		)
 	async def receive(self, text_data):
 		text_data_json = json.loads(text_data)
--- newsapp/chat/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-
- 
-@login_required
-def chatPage(request, *args, **kwargs):
-    context = {}
-    return render(request, "chatpage.html", context)
-
